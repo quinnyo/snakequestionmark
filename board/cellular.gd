159d65183879dbbc14ae6fba164b4cd3fb419433
Quinn <3379314+quinnyo@users.noreply.github.com>
2024-10-27 07:15:22 +1000

    Replace Cellular dirty flag with immediate position update

--- board/cellular.gd
+++ board/cellular.gd
@@ -1,20 +1,18 @@
 class_name Cellular extends Node2D
 
-
 @export var cpos: Vector2i:
 	set(value):
 		cpos = value
-		_dirty = true
+		refresh()
+
 
 var _board: Board2D
-var _dirty: bool = true
+
+
+func refresh() -> void:
+	if _board:
+		global_transform = _board.global_transform.translated_local(_board.cell_centre(cpos))
 
 
 func _enter_tree() -> void:
 	_board = Board2D.find_parent_board(self)
-
-
-func _process(_delta: float) -> void:
-	if _dirty && _board:
-		global_transform = _board.global_transform.translated_local(_board.cell_centre(cpos))
-		_dirty = false
