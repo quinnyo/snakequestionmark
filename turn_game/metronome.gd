86f31bcd834c7ec07c04ecfeada2ff2750665d2e
Quinn <3379314+quinnyo@users.noreply.github.com>
2024-10-31 07:04:16 +1000

    add subdivisions to Metronome

--- turn_game/metronome.gd
+++ turn_game/metronome.gd
@@ -5,10 +5,12 @@ const PROCESS_PRIORITY := -9
 
 
 signal tick(n: int)
+signal beat(bar: int, b: int)
 
 
 ## time between ticks in seconds
 @export var period: float = 0.3
+@export var bar_div: int = 4
 ## scales (multiplies) the accumulated input time
 @export var time_scale: float = 1.0
 ## maximum number of ticks to process at once
@@ -20,10 +22,20 @@ signal tick(n: int)
 
 @export_storage var _t_accum: float = 0.0
 @export_storage var _tick_count: int = 0
+@export_storage var _bar_count: int = 0
 
 
 func finish_tick() -> void:
 	_t_accum = period
+
+
+func _tick() -> void:
+	tick.emit(_tick_count)
+	var b := _tick_count % bar_div
+	beat.emit(_bar_count, b)
+	_tick_count += 1
+	if _tick_count % bar_div == 0:
+		_bar_count += 1
 
 
 func _ready() -> void:
@@ -32,13 +44,13 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	var _beat_period := period / float(bar_div)
 	_t_accum += delta * time_scale
 	if max_overs >= 0:
-		_t_accum = minf(float(1 + max_overs) * period, _t_accum)
-	var ticks := floori(_t_accum / period)
+		_t_accum = minf(float(1 + max_overs) * _beat_period, _t_accum)
+	var ticks := floori(_t_accum / _beat_period)
 	if max_ticks >= 0:
 		ticks = mini(max_ticks, ticks)
 	for _i in range(ticks):
-		_t_accum -= period
-		tick.emit(_tick_count)
-		_tick_count += 1
+		_t_accum -= _beat_period
+		_tick()
