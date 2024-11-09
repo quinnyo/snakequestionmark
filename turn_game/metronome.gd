f5f9d7dadacc4dae9755aa454b7e177a87aed3db
Quinn <3379314+quinnyo@users.noreply.github.com>
2024-11-10 00:08:24 +1000

    Add play/pause/stop control to Metronome

--- turn_game/metronome.gd
+++ turn_game/metronome.gd
@@ -1,12 +1,14 @@
 class_name Metronome extends Node
 
+enum RunningState { IDLE, RUNNING, PAUSED, STOPPED, _INIT }
+
 ## Default value of `process_priority`.
 const PROCESS_PRIORITY := -9
-
 
 signal tick(t: int, bar: int, beat: int)
 
 
+@export var autostart: bool = true
 ## time between ticks in seconds
 @export var period: float = 0.3
 @export var bar_div: int = 4
@@ -19,6 +21,7 @@ signal tick(t: int, bar: int, beat: int)
 ## if true, keep the configured (e.g. in scene) value of `process_priority`
 @export var custom_process_priority: bool = false
 
+@export_storage var _state: RunningState = RunningState._INIT
 @export_storage var _t_accum: float = 0.0
 @export_storage var _tick_count: int = 0
 @export_storage var _bar_count: int = 0
@@ -26,6 +29,27 @@ signal tick(t: int, bar: int, beat: int)
 
 func finish_tick() -> void:
 	_t_accum = period
+
+
+func start() -> void:
+	if _state == RunningState.STOPPED:
+		reset()
+	_state = RunningState.RUNNING
+
+
+func pause() -> void:
+	_state = RunningState.PAUSED
+
+
+func stop() -> void:
+	_state = RunningState.STOPPED
+
+
+func reset() -> void:
+	_state = RunningState.IDLE
+	_t_accum = 0.0
+	_tick_count = 0
+	_bar_count = 0
 
 
 func _tick() -> void:
@@ -39,9 +63,14 @@ func _tick() -> void:
 func _ready() -> void:
 	if !custom_process_priority:
 		process_priority = PROCESS_PRIORITY
+	if _state == RunningState._INIT && autostart:
+		start()
 
 
 func _process(delta: float) -> void:
+	if _state != RunningState.RUNNING:
+		return
+
 	var _beat_period := period / float(bar_div)
 	_t_accum += delta * time_scale
 	if max_overs >= 0:
