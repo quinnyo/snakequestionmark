4a0a44355d78b48ddb75ff60a850298362dd79fe
Quinn <3379314+quinnyo@users.noreply.github.com>
2024-11-11 13:07:15 +1000

    impl Compact phase

--- turn_game/snake_game/compact.gd
+++ turn_game/snake_game/compact.gd
@@ -1,9 +1,26 @@
 class_name XtgCompact extends XtgPhase
 
 
+const CellShift := preload("res://board/extra/cell_shift.gd")
+
+var cell_shift: CellShift
+
+
 func _tg_activate() -> bool:
 	return true
 
 
+func _tg_start() -> void:
+	var board := get_board()
+	cell_shift = CellShift.new()
+	cell_shift.board = board
+	cell_shift.prepare()
+	if !cell_shift.has_work():
+		status = Status.BREAK
+
+
 func _tg_tick() -> void:
-	status = Status.BREAK
+	if cell_shift.has_work():
+		cell_shift.perform()
+	else:
+		status = Status.COMPLETE
